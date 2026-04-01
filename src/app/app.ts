8ce86d2cac2ec86e63422c8db2af101ef58355e2
ML510 <mr.mahesh.303@gmail.com>
2026-04-01 15:44:11 +0530

    Dashboard Create

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Login } from './page/login/login';
+import { Dashboard } from "./page/dashboard/dashboard";
+import { Candidate } from "./page/candidate/candidate";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, Dashboard, Candidate],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
